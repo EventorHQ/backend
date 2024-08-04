a35331783ad7bc17d28fab053a6cdf58cdd0b2c5
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-08-04 05:43:34 +0300

    refactor: remove bot project

--- bot/src/bot.ts
+++
@@ -1,9 +0,0 @@
-import { Bot } from "grammy";
-import { BOT_TOKEN } from "./config/config";
-
-const bot = new Bot(BOT_TOKEN);
-
-bot.on("message", (ctx) => ctx.reply("Hi there!"));
-
-console.log("Bot started");
-bot.start();
